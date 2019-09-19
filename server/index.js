961e3776c581a4c9dd37327f59ee9df8bf3bb4f1
davidrpolk <davidrpolk@gmail.com>
2019-09-18 19:52:52 -0500

    building notes functions

--- server/index.js
+++ server/index.js
@@ -202,7 +202,7 @@ app.get('/hotList', (req, res) => {
 });
 
 // connection for recipe notes
-app.post('/api/Notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
   models.UsersRecipes.update(
     { notes: req.body.note },
     { returning: true, where: { userId: req.body.userId, recipeId: req.body.recipeId } },
@@ -214,6 +214,10 @@ app.post('/api/Notes', (req, res) => {
       console.log(err);
     });
 });
+
+app.get('/api/notes', (req, res) => {
+  
+})
 
 app.post('/api/groceryList', (req, res) => {
   models.groceryList.create({
