03ae2fcbfc209737bb66a583669d5d871f9c1488
Carin Chapman <Chapman.carin@gmail.com>
2019-09-19 17:13:45 -0500

    Add axios.get for req all saved notes on recipes from server

--- react-client/src/components/FavRecipeItem.jsx
+++ react-client/src/components/FavRecipeItem.jsx
@@ -24,7 +24,7 @@ class FavRecipeItem extends Component {
     super(props);
     this.state ={ 
       collapse: false,
-      notes: ['fack sata', 'sckpada', 'trashPandas'],
+      notes: '',
       newNote: '',
     };
     this.toggleNotes = this.toggleNotes.bind(this);
@@ -32,6 +32,18 @@ class FavRecipeItem extends Component {
     this.saveRecipeNotes = this.saveRecipeNotes.bind(this);
   }
   // const { user, removeFromFavorites } = this.props;
+
+  componentDidMount() {
+    Axios.get(`/api/notes?userId=${this.props.user.id}&recipeId=${this.props.favRecipe.id}`)
+      .then(usersNotes => {
+        // const { notes } = this.state;
+        // notes.unshift(usersNotes.data);
+        console.log(usersNotes)
+        this.setState({
+          notes: usersNotes.data,
+        })
+      })
+  }
   
   removeFavoritesAndRedirect (selectedRecipe) {
     this.props.removeFromFavorites(selectedRecipe)
